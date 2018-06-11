7b9d7937b79b3db7f3cbb8162a5bbd0fdb94f841
Oluwatoyin Oshinkoya <dotunoshinkoya@yahoo.com>
2018-06-11 13:11:17 -0700

    files included

---
+++ clicky_game/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p> &copyCopyright 2018 Oluwatoyin,</p>
+	</header>
+);
+
+export default Footer;
---
+++ clicky_game/src/components/FriendCard/FriendCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FriendCard.css";
+
+const FriendCard = props => (
+  <div className="card">
+    <div className="img-container">
+      
+  </div>    
+  </div>
+);
+
+export default FriendCard;
